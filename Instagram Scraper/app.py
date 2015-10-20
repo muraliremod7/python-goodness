2286ac39ae82b93a5736316aed37ddf07cadd989
Nzube <okoyenzube@ymail.com>
2015-10-20 23:53:31 +0100

    add download_image() function

--- Instagram Scraper/app.py
+++ Instagram Scraper/app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,request 
 import requests
+import urllib.request
 from bs4 import BeautifulSoup
 import json
 import os
@@ -16,17 +17,32 @@ def index():
 		info=data[0]
 		json_data=json.loads(info[21:-1])
 
-
-
 		final_data=json_data['entry_data']['PostPage']
 		final=final_data[0]
 		image=final['media']['display_src']
 
 		while image!='':
 			print(image)
+			download_photo(image, "1.jpg")
+			print("image saved")
 			return "<img src="+image+" align='center'>"
+			
+			return render_template('index.html')
 
-	return render_template('index.html')
+
+def download_photo(img_url, filename):
+        image_on_web = urllib.urlopen(img_url)
+        if image_on_web.headers.maintype == 'image':
+            buf = image_on_web.read()
+            path = os.getcwd() + "/img"
+            file_path = "%s%s" % (path, filename)
+            downloaded_image = file(file_path, "wb")
+            downloaded_image.write(buf)
+            downloaded_image.close()
+            image_on_web.close()
+        else:
+            return False    
+
 
 if __name__=='__main__':
 	app.run()
