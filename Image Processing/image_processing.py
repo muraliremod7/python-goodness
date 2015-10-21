61e8a47ef20bd2939826210a3f3f9c3dca2bcea2
Nzube <okoyenzube@ymail.com>
2015-10-21 20:56:46 +0100

    add image resize function

--- Image Processing/image_processing.py
+++ Image Processing/image_processing.py
@@ -6,20 +6,22 @@ name = "testimage.jpeg"
 folder = "assets"+"/"
 
 
-"""
-Function: Sharpen Image
-args: path-Image
-"""
-
+#Sharepn Image Function
 def sharpen_img(path,img_name):
 	img = Image.open(path+img_name)
 	sharp_img = img.filter(ImageFilter.SHARPEN)
 	sharp_img.save("sharp_"+img_name,'JPEG')
 
+#Resize Image Function
+def resize_img(img_name,length,width):
+	img = Image.open(img_name)
+	resized_img = img.resize((width,length))
+	resized_img.save("resized_"+img_name,'JPEG')
 
 def exif():
 	exifdata = im._getexif()
 	print(exifdata)
 
 
-sharpen_img(folder,name)
+#Run Functions
+resize_img("testimage.jpeg",650,500)
