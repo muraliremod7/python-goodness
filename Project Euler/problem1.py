bb0262bc4a9becd08eb8ff7493dee1b1be57cdb0
Nzube <okoyenzube@ymail.com>
2015-10-22 21:54:57 +0100

    :page_with_curl: Python 3 solution to http://projecteuler.net/problem=1

--- Project Euler/problem1.py
+++ Project Euler/problem1.py
@@ -7,3 +7,13 @@ that are multiples of 3 or 5, we get 3, 5, 6 and 9.
 The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000. """
 
+
+def sum_of_multiples_of_3_and_5(number):
+    sum = 0
+    for i in range(number):
+        if i % 3 == 0 or i % 5 == 0:
+            sum += i
+    return sum
+
+#Test
+print (sum_of_multiples_of_3_and_5(1000))
