c3bf92c51615a86acf0c0c89c18f46c556e771e6
Nzube <okoyenzube@ymail.com>
2015-10-22 11:12:50 +0100

    :shipit: Implement Image Rotation

--- Image Processing/image_processing.py
+++ Image Processing/image_processing.py
@@ -5,26 +5,35 @@ from PIL import Image, ImageFilter
 name = "testimage.jpeg"
 folder = "assets"+"/"
 
-
+#Open Image
+#returns the Image object
+def open_img(img_name):
+	img = Image.open(img_name)
+	return img
 
 #Sharpen Image Function
 def sharpen_img(path,img_name):
-	img = Image.open(path+img_name)
+	img = open_img(img_name)
 	sharp_img = img.filter(ImageFilter.SHARPEN)
 	sharp_img.save("sharp_"+img_name,'JPEG')
 
 #Resize Image Function
 def resize_img(img_name,length,width):
-	img = Image.open(img_name)
+	img = open_img(img_name)
 	resized_img = img.resize((width,length))
 	resized_img.save("resized_"+img_name,'JPEG')
 
 #Get Image Exif Data
 def exif(img_name):
-	img = Image.open(img_name)
+	img = open_img(img_name)
 	exifdata = img._getexif()
 	print(exifdata)
 
+#Rotate Image
+def rotate_img(img_name,degrees):
+	img = open_img(img_name)
+	rotated_img = img.rotate(degrees)
+	rotated_img.save("rotated_"+img_name,'JPEG')
 
 #Run Functions
-resize_img("testimage.jpeg",650,500)
+resize_img("testimage.jpeg",40,30)
