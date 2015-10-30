fc4c9b1f1faac0df1cde03bf936738b10218faac
Nzube <okoyenzube@ymail.com>
2015-10-30 22:17:55 +0100

    :page_with_curl: Add read.py reading text from and external file

--- FileIO/read.py
+++ FileIO/read.py
@@ -1,19 +1,19 @@
-#Reading contents from a file in python
-import os.path
- 
-# Define a filename.
+#Reading file in python
+
+import os
+
+#filename to read
 filename = "readfile.txt"
- 
+
+#check if file exists
 if not os.path.isfile(filename):
     print('File does not exist.')
+
 else:
-    # Open the file as f. 
-    # The function readlines() reads the file.
-    with open(filename) as f:
-        content = f.read().splitlines()
- 
-    # Show the file contents line by line.
-    # We added the comma to print single newlines and not double newlines.
-    # This is because the lines contain the newline character '\n'. 
-    for line in content:
-        print(line)
+	#read file
+	with open(filename) as f: 
+		content = f.read().splitlines()
+
+		#show the file contents line by line
+		for line in content:
+			print(line)
