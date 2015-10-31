7f5dd1e93677f838e343f9b5052481057253b231
Nzube <okoyenzube@ymail.com>
2015-10-31 18:36:33 +0100

    :tada: Array operations using python

---
+++ FileIO/lists.py
@@ -0,0 +1,33 @@
+#-----------------------------
+#Array operations using python
+#-----------------------------
+#Printing elements at index
+#appending elements to array
+#sorting elements in array
+#reverse elements of an array
+
+#define array
+l = ["Drake","Lil Wayne","Tyga","Birdman"]
+
+#print all the elements of the array
+print(l)
+
+
+#print array item at index
+print(l[0])
+print(l[1])
+
+#append elements to array
+print(l)
+l.append("Justin")
+print(l)
+
+#sort elements of the array
+print(l)
+l.sort
+print(l)
+
+#reverse elements of the array
+print(l)
+l.reverse()
+print(l)
