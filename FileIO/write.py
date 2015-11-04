1a368b823ddb0c73ecf078bcbc4c2cedbabad7bc
Nzube <okoyenzube@ymail.com>
2015-11-04 23:10:56 +0100

    :page_with_curl: comment error fix

--- FileIO/write.py
+++ FileIO/write.py
@@ -9,7 +9,7 @@ if not os.path.isfile(filename):
     print('File does not exist.')
 
 else:
-	#the a file tells python to keep the files content
+	#the a flag tells python to keep the files content
 	#and append(add line) to the end of the file
 	myfile = open(filename,'w')
 	myfile.write("This line of text was add with python")
