e9a9373af393aa191f6dc094c506ad6af5e9555d
Nzube <okoyenzube@ymail.com>
2015-11-04 23:09:34 +0100

    :page_with_curl: use loops to print 200 random cell numbers to writefile.txt

---
+++ FileIO/looping.py
@@ -0,0 +1,23 @@
+#Learning how to use loops in python :)
+#print random cell phone numbers
+#======================================
+
+import random 	#import the random module
+import os 		#import the os module
+
+filename = "writefile.txt" #name of the file to write to
+
+start_list = ["0813","0803","0809","0903","0818"] 	#number start codes
+
+if not os.path.isfile(filename):
+	print("file not found")
+
+for i in range(1,200):
+	x = random.randint(10000000,99999999)
+	b = random.randint(0,len(start_list)-1)	#select random item from list
+	myfile = open(filename,"a")
+	myfile.write(start_list[b]+"-"+str(x))
+	myfile.write("\n")
+	myfile.close()
+
+print("Writing completed")
